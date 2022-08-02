a3d80ff6f732ae93beda4f00a81e237b9e510251
PeytonCast <107663364+PeytonCast@users.noreply.github.com>
2022-08-02 19:46:36 -0400

    Update script.js
    In this commit, I added code to handle time events and I added a display date and time to the jumbotron.

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,44 @@
 //start with the list of vars connected to the html elements 
 //note i will be using jQueary for this project 
+var m = moment()
+var currentTime = $('#cur-time');
+var currentDay = $('#cur-day');
+var nine = $('#9am');
+var ten = $('#10am');
+var elev = $('#11am');
+var twelv = $('#12pm');
+var one = $('#1pm');
+var two = $('#2pm');
+var three = $('#3pm');
+var four = $('#4pm');
+var five = $('#5pm');
+
+
+//this will highlight the current hour
+if (m.format('h') == 9) {nine.addClass('bg-danger')}      
+if (m.format('h') == 10) {ten.addClass('bg-danger')}
+if (m.format('h') == 11) {elev.addClass('bg-danger')}
+if (m.format('h') == 12) {twelv.addClass('bg-danger')}
+if (m.format('h') == 1) {one.addClass('bg-danger')}
+if (m.format('h') == 2) {two.addClass('bg-danger')}
+if (m.format('h') == 3) {three.addClass('bg-danger')}
+if (m.format('h') == 4) {elev.addClass('bg-danger')}
+if (m.format('h') == 5) {five.addClass('bg-danger')}
+
+
+
+
+
+
+
 
 //next i will need the current day/date and time to display in the header 
-
-
-//i will need the hours divided up into an 8 hour day
+currentTime.text(m.format('LT'))
+currentDay.text(m.format('[Today is] LL'))
+//i will need the hours divided up into an 9 hour day
 //these hour blocks will need to be standard buisness hours
 // the current hour will need to be a light blue (cuz it would look nicer than danger red)
+
 //the passed hours will need to be grey
 //next the future hours will need to be green
 //this means i will need to CREATE HTML ELEM
@@ -22,3 +54,4 @@
 //and last but not least the save button
 //it looks like in the gif they are using a photo on the btn so look into that
 //if that doesnt workout than just use words
+
