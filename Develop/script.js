1c1c85d014be4446560b9d819dbae268166df15f
PeytonCast <107663364+PeytonCast@users.noreply.github.com>
2022-08-02 13:51:02 -0400

    Update script.js
    JavaScript logic notes to get me going.

---
+++ Develop/script.js
@@ -0,0 +1,24 @@
+//start with the list of vars connected to the html elements 
+//note i will be using jQueary for this project 
+
+//next i will need the current day/date and time to display in the header 
+
+
+//i will need the hours divided up into an 8 hour day
+//these hour blocks will need to be standard buisness hours
+// the current hour will need to be a light blue (cuz it would look nicer than danger red)
+//the passed hours will need to be grey
+//next the future hours will need to be green
+//this means i will need to CREATE HTML ELEM
+
+//IF i input an appointment/event into an hour block,
+//THEN that input will stay there via local storage 
+//and add a message in green saying save was sucsessfull 
+//this means i will need a save button on each hour block 
+
+//meaning the HTML elements created will need to have the including 
+//a label with a var of time 
+//an imput field that doubles as a display text field with a class
+//and last but not least the save button
+//it looks like in the gif they are using a photo on the btn so look into that
+//if that doesnt workout than just use words
