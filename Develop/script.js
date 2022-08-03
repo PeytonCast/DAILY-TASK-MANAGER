bfd34612240ed20163c8dbc90b2c2f45cc079a40
PeytonCast <107663364+PeytonCast@users.noreply.github.com>
2022-08-03 01:19:57 -0400

    Update script.js
    Now the past, present, and future are color-coded. Next, I will add the local storage to the textbox. Will it work? I have no idea. As a wise mentor once said "We'll burn that bridge when we come to it"... it's late...I'm going to bed lol.

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,4 @@
-//start with the list of vars connected to the html elements 
+//DONE start with the list of vars connected to the html elements 
 //note i will be using jQueary for this project 
 var m = moment()
 var currentTime = $('#cur-time');
@@ -12,36 +12,131 @@ var two = $('#2pm');
 var three = $('#3pm');
 var four = $('#4pm');
 var five = $('#5pm');
-
-
-//this will highlight the current hour
-if (m.format('h') == 9) {nine.addClass('bg-danger')}      
-if (m.format('h') == 10) {ten.addClass('bg-danger')}
-if (m.format('h') == 11) {elev.addClass('bg-danger')}
-if (m.format('h') == 12) {twelv.addClass('bg-danger')}
-if (m.format('h') == 1) {one.addClass('bg-danger')}
-if (m.format('h') == 2) {two.addClass('bg-danger')}
-if (m.format('h') == 3) {three.addClass('bg-danger')}
-if (m.format('h') == 4) {elev.addClass('bg-danger')}
-if (m.format('h') == 5) {five.addClass('bg-danger')}
-
-
-
-
-
-
-
-
-//next i will need the current day/date and time to display in the header 
+var currentHour = m.format('h')
+//DONE next i will need the current day/date and time to display in the header 
 currentTime.text(m.format('LT'))
 currentDay.text(m.format('[Today is] LL'))
-//i will need the hours divided up into an 9 hour day
-//these hour blocks will need to be standard buisness hours
-// the current hour will need to be a light blue (cuz it would look nicer than danger red)
+
+//adds classes to colorcode the hour-blocks 
+//changes 9am to red to highlight current hour
+if (currentHour == 9) {
+five.addClass('bg-success')
+four.addClass('bg-success')
+three.addClass('bg-success')
+two.addClass('bg-success')
+one.addClass('bg-success')
+twelv.addClass('bg-success')
+elev.addClass('bg-success')
+ten.addClass('bg-success');
+nine.addClass('bg-info')} 
+
+//changes 10am to red to highlight current hour
+if (currentHour == 10) {
+five.addClass('bg-success')
+four.addClass('bg-success')
+three.addClass('bg-success')
+two.addClass('bg-success')
+one.addClass('bg-success')
+twelv.addClass('bg-success')
+elev.addClass('bg-success')
+ten.addClass('bg-info');
+nine.addClass('bg-dark')};
+
+//changes 11am to red to highlight current hour
+if (currentHour == 11) {
+five.addClass('bg-success')
+four.addClass('bg-success')
+three.addClass('bg-success')
+two.addClass('bg-success')
+one.addClass('bg-success')
+twelv.addClass('bg-success')
+elev.addClass('bg-info')
+ten.addClass('bg-dark');
+nine.addClass('bg-dark')}
+
+//changes 12pm to red to highlight current hour
+if (currentHour == 12) {
+five.addClass('bg-success')
+four.addClass('bg-success')   
+three.addClass('bg-success')
+two.addClass('bg-success')
+one.addClass('bg-success')
+twelv.addClass('bg-danger')
+elev.addClass('bg-dark')
+ten.addClass('bg-dark')
+nine.addClass('bg-dark')}
+
+//changes 1pm to red to highlight current hour
+if (currentHour == 1) {
+five.addClass('bg-success')
+four.addClass('bg-success')
+three.addClass('bg-success')
+two.addClass('bg-success')
+one.addClass('bg-danger')
+twelv.addClass('bg-dark')
+elev.addClass('bg-dark')
+ten.addClass('bg-dark')
+nine.addClass('bg-dark')}
+
+//changes 2pm to red to highlight current hour
+if (currentHour == 2) {
+five.addClass('bg-success')
+four.addClass('bg-success')
+three.addClass('bg-success')
+two.addClass('bg-danger')
+one.addClass('bg-dark')
+twelv.addClass('bg-dark')
+elev.addClass('bg-dark')
+ten.addClass('bg-dark')
+nine.addClass('bg-dark')}
+
+//changes 3pm to red to highlight current hour
+if (currentHour == 3) {
+five.addClass('bg-success')
+four.addClass('bg-success')
+three.addClass('bg-danger')
+two.addClass('bg-dark')
+one.addClass('bg-dark')
+twelv.addClass('bg-dark')
+elev.addClass('bg-dark')
+ten.addClass('bg-dark')
+nine.addClass('bg-dark')}
+
+//changes 4pm to red to highlight current hour
+if (currentHour == 4) {
+five.addClass('bg-success')
+four.addClass('bg-danger')
+three.addClass('bg-dark')
+two.addClass('bg-dark')
+one.addClass('bg-dark')
+twelv.addClass('bg-dark')
+elev.addClass('bg-dark')
+ten.addClass('bg-dark')
+nine.addClass('bg-dark')}
+
+//changes 5pm to red to highlight current hour
+if (currentHour == 5) {
+five.addClass('bg-danger')
+four.addClass('bg-dark')
+three.addClass('bg-dark')
+two.addClass('bg-dark')
+one.addClass('bg-dark')
+twelv.addClass('bg-dark')
+elev.addClass('bg-dark')
+ten.addClass('bg-dark')
+nine.addClass('bg-dark')}
+
+
+//THIS IS WHERE THE LOCAL STORAGE CODE WILL BE
+
+
+
+
+
 
 //the passed hours will need to be grey
 //next the future hours will need to be green
-//this means i will need to CREATE HTML ELEM
+
 
 //IF i input an appointment/event into an hour block,
 //THEN that input will stay there via local storage 
