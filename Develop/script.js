e9db079f0cf969d9d11e7750619afb554862db64
PeytonCast <107663364+PeytonCast@users.noreply.github.com>
2022-08-03 16:37:24 -0400

    Update script.js
    in this commit, I wire the new clear btn and the Refresh btn. I also completed the local storage so I would say that this is completely operational now. Just need to update the CSS and write a readme... I'm sure I'm forgetting something but we're real close.

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,8 @@
-//DONE start with the list of vars connected to the html elements 
-//note i will be using jQueary for this project 
+// HANDLES THE TIME IN THE HEADER
 var m = moment()
 var currentTime = $('#cur-time');
 var currentDay = $('#cur-day');
+//THESE ARE CONECTED TO EACH HOUR BLOCK's LABEL
 var nine = $('#9am');
 var ten = $('#10am');
 var elev = $('#11am');
@@ -12,6 +12,22 @@ var two = $('#2pm');
 var three = $('#3pm');
 var four = $('#4pm');
 var five = $('#5pm');
+
+//CONNECTED TO REFRESH BTN
+var refresh = $('#refresh')
+//a button to update the time
+refresh.on('click', function () {
+    location.reload();
+  });
+
+//CLEAR BTN
+var clear = $('#clear')
+clear.on('click', function () {
+    localStorage.clear()
+    location.reload();
+  });
+
+//SIMPLIFIES IF STATEMENTS
 var currentHour = m.format('h')
 //DONE next i will need the current day/date and time to display in the header 
 currentTime.text(m.format('LT'))
@@ -128,25 +144,113 @@ nine.addClass('bg-dark')}
 
 
 //THIS IS WHERE THE LOCAL STORAGE CODE WILL BE
+//these var are for the textboxes that will store localy
+//ALL TEXTBOXES
+var nineText =$('#9am-text')
+var tenText =$('#10am-text')
+var elevText =$('#11am-text')
+var twelvText =$('#12pm-text')
+var oneText =$('#1pm-text')
+var twoText =$('#2pm-text')
+var threeText =$('#3pm-text')
+var fourText =$('#4pm-text')
+var fiveText =$("#5pm-text")
+var refresh = $("#refresh")
+
+//ALL SAVE BTNS
+var saveNine = $("#save9")
+var saveTen = $("#save10")
+var saveElev = $("#save11")
+var saveTwelv = $("#save12")
+var saveOne = $("#save1")
+var saveTwo = $("#save2")
+var saveThree = $("#save3")
+var saveFour = $("#save4")
+var saveFive = $("#save5")
+
+//ALL EVENT P TAGS 
+var eventNine = $("#event9")
+var eventTen = $("#event10")
+var eventEleve = $("#event11")
+var eventTwelv = $("#event12")
+var eventOne = $("#event1")
+var eventTwo = $("#event2")
+var eventThree = $("#event3")
+var eventFour = $("#event4")
+var eventFive = $("#event5")
+
+//these var are for the textboxes that will store localy
+var nineText =$('#9am-text')
+var tenText =$('#10am-text')
+var elevText =$('#11am-text')
+var twelvText =$('#12pm-text')
+var oneText =$('#1pm-text')
+var twoText =$('#2pm-text')
+var threeText =$('#3pm-text')
+var fourText =$('#4pm-text')
+var fiveText =$('#5pm-text')
+
+
+//HANDLES STORAGE FOR 9AM BLOCK
+nineText.on('input', name=>{eventNine.text(name.target.value) })
+var save9 = () => {localStorage.setItem('event9', eventNine.text())}
+saveNine.on('click', save9)
+eventNine.text(localStorage.getItem('event9'))
+
+//HANDLES STORAGE FOR 10AM BLOCK
+tenText.on('input', name=>{eventTen.text(name.target.value) })
+var save10 = () => {localStorage.setItem('event10', eventTen.text())}
+saveTen.on('click', save10)
+eventTen.text(localStorage.getItem('event10'))
+
+//HANDLES STORAGE FOR 11AM BLOCK
+elevText.on('input', name=>{eventEleve.text(name.target.value)})
+var save11 = () => {localStorage.setItem('event11', eventEleve.text())}
+saveElev.on('click', save11)
+eventEleve.text(localStorage.getItem('event11'))
+
+//HANDLES STORAGE FOR 12PM BLOCK
+twelvText.on('input', name=>{eventTwelv.text(name.target.value)})
+var save12 = () => {localStorage.setItem('event12', eventTwelv.text())}
+saveTwelv.on('click', save12)
+eventTwelv.text(localStorage.getItem('event12'))
+
+//HANDLES STORAGE FOR 1PM BLOCK
+oneText.on('input', name=>{eventOne.text(name.target.value)})
+var save1 = () => {localStorage.setItem('event1', eventOne.text())}
+saveOne.on('click', save1)
+eventOne.text(localStorage.getItem('event1'))
+
+//HANDLES STORAGE FOR 1PM BLOCK
+oneText.on('input', name=>{eventOne.text(name.target.value)})
+var save1 = () => {localStorage.setItem('event1', eventOne.text())}
+saveOne.on('click', save1)
+eventOne.text(localStorage.getItem('event1'))
+
+//HANDLES STORAGE FOR 2PM BLOCK
+twoText.on('input', name=>{eventTwo.text(name.target.value)})
+var save2 = () => {localStorage.setItem('event2', eventTwo.text())}
+saveTwo.on('click', save2)
+eventTwo.text(localStorage.getItem('event2'))
+
+//HANDLES STORAGE FOR 3PM BLOCK
+threeText.on('input', name=>{eventThree.text(name.target.value)})
+var save3 = () => {localStorage.setItem('event3', eventThree.text())}
+saveThree.on('click', save3)
+eventThree.text(localStorage.getItem('event3'))
+
+//HANDLES STORAGE FOR 4PM BLOCK
+fourText.on('input', name=>{eventFour.text(name.target.value)})
+var save4 = () => {localStorage.setItem('event4', eventFour.text())}
+saveFour.on('click', save4)
+eventFour.text(localStorage.getItem('event4'))
+
+//HANDLES STORAGE FOR 5PM BLOCK
+fiveText.on('input', name=>{eventFive.text(name.target.value)})
+var save5 = () => {localStorage.setItem('event5', eventFive.text())}
+saveFive.on('click', save5)
+eventFive.text(localStorage.getItem('event5'))
 
 
 
-
-
-
-//the passed hours will need to be grey
-//next the future hours will need to be green
-
-
-//IF i input an appointment/event into an hour block,
-//THEN that input will stay there via local storage 
-//and add a message in green saying save was sucsessfull 
-//this means i will need a save button on each hour block 
-
-//meaning the HTML elements created will need to have the including 
-//a label with a var of time 
-//an imput field that doubles as a display text field with a class
-//and last but not least the save button
-//it looks like in the gif they are using a photo on the btn so look into that
-//if that doesnt workout than just use words
 
